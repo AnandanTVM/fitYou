e6ad8806f7efd564bcf9f2ea8b0e848e83eec8d7
Anandan <anandan2016@gmail.com>
2022-12-02 16:34:24 +0530

    eslint included in project

--- Server/controllers/homeControllers.js
+++ Server/controllers/homeControllers.js
@@ -1,25 +1,26 @@
-const { response } = require('express')
-const json = require('express')
-const AsyncHandler = require('express-async-handler')
-const homeHelper = require('../helpers/homeHelpers')
-
-
+/* eslint-disable prettier/prettier */
+// const { response } = require('express');
+// const json = require('express');
+const AsyncHandler = require('express-async-handler');
+const homeHelper = require('../helpers/homeHelpers');
 
 const clientSignup = AsyncHandler(async (req, res) => {
-    let data = req.body
-    //remove unwated feild from object
-    delete data.cpassword
-    homeHelper.doClientSignup(data).then((response) => {
-        if (response.phoneFound) {
-            res.json({ status: 'error', error: 'Duplicate Phone number' })
-        } else {
-            res.json({ status: "success" })
-        }
-    }).catch((err) => {
-        console.log(err);
+  const data = req.body;
+  // remove unwated feild from object
+  delete data.cpassword;
+  homeHelper
+    .doClientSignup(data)
+    .then((response) => {
+      if (response.phoneFound) {
+        res.json({ status: 'error', error: 'Duplicate Phone number' });
+      } else {
+        res.json({ status: 'success' });
+      }
     })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
-})
-
-//exports
+// exports
 exports.clientSignup = clientSignup;
